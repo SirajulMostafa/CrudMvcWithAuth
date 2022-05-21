f86b4ef351272e396c70b2fa4f3c35ab318f4e17
sirajul Mostafa <sirajulmost@gmail.com>
2022-05-21 23:23:39 +0600

    init

---
+++ src/main/java/com/sirajul/CrudMvcWithAuth/CrudMvcWithAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sirajul.CrudMvcWithAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMvcWithAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMvcWithAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirajul/CrudMvcWithAuth/CrudMvcWithAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirajul.CrudMvcWithAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMvcWithAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
